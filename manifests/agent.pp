2477fc5ad6d6e418c437122997831391ca7653d9
Zack Smith <zack@puppetlabs.com>
2014-10-20 11:30:18 -0700

    Add full ec2-user support

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -5,11 +5,16 @@ class clamps::agent (
   $amqpass = 'password',
 ) {
 
-  user {'ec2-user':
-      ensure => present,
-      uid    => 500,
-   }
-
+  user { 'ec2-user':
+    ensure           => 'present',
+    gid              => '500',
+    home             => '/home/ec2-user',
+    password         => '!!',
+    password_max_age => '99999',
+    password_min_age => '0',
+    shell            => '/bin/bash',
+    uid              => '500',
+  }
 
   $nonroot_usernames = clamps_users($nonroot_users)
 
