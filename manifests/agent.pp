e3d4db6842b8c6c9d2bdfffddb44dcbd7f27a0a6
Zack Smith <zack@puppetlabs.com>
2014-10-21 17:22:35 +0000

    Add comments

--- manifests/agent.pp
+++ manifests/agent.pp
@@ -11,11 +11,18 @@ class clamps::agent (
     servername => $master,
     ca_server  => $ca,
   }
+
+
+  # This will not allow the "main" mcollective to start as
+  # it simply checks for a process named mcollective.
+  # The status override in the service resource makes the
+  # non-root nodes work though
   ::clamps::mcollective { $nonroot_usernames: 
     amqserver => $master,
     amqpass   => $amqpass,
   }
 
+  # Need to manage the ec2-user if you enabled this
   #resources {'user':
   #  purge              => true,
   #  unless_system_user => true,
