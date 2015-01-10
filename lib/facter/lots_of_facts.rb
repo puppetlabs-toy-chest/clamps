8df8b71bd5d8b0f431e98bcc339f6515135649e0
Eric Williamson <eric.williamson@puppetlabs.com>
2015-01-09 17:32:10 -0800

    (QENG-1640) Limit number of facts generate
    Previous to this commit, the random facts generated per agent would
    increase with each puppet agent run, due to the fact names also being
    randomly created.
    
    This commit changes it to generate consistent fact names, but allow the
    user to configure the number of them to see where the limits are.
    
    Further, it will also randomize the data for the facts on each run and
    vary the length of the facts, to simulate users having facts like SSH
    keys or other random data.

--- lib/facter/lots_of_facts.rb
+++ lib/facter/lots_of_facts.rb
@@ -1,14 +1,21 @@
-# generate a random hash with a lot of crap in it, turn it into facts
+# To compensate for our "fake" agents having virtually no facts,
+# generate some random facts. To better stress test puppetdb, (avoid its data
+# deduplication) these facts should:
+#
+# contain different data each run
+# have the same fact name so we don't end up with 4 million facts in puppetdb
+# have verying length, to simulate things like SSH keys.
 
 require 'securerandom'
 require 'facter'
 
-$num_of_facts = rand(5..75)
-
+num_facts = File.exist?('/etc/puppetlabs/num_facts') ? File.read('/etc/puppetlabs/num_facts') : 500
 hash_of_facts = Hash.new {|h,k| h[k] = [] }
 
-for i in 1..$num_of_facts
-  hash_of_facts[SecureRandom.hex] << SecureRandom.hex
+for i in 1..num_facts
+  fact_value = ""
+  rand(1..50).times { fact_value << SecureRandom.hex }
+  hash_of_facts["fact_#{i}"] << fact_value
 end
 
 hash_of_facts.each_pair do |factname, factvalue|
