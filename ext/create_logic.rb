f2beb1eec3aff371a85e5dd8bb2527daf15c2f02
Rick Bradley <rick@rickbradley.com>
2015-06-23 12:22:16 -0500

    (QENG-2574) Generate logic files that work with future parser
    Prior to this, recent puppets (using the future parser) would throw errors about the
    output of fqdn_rand outputting basically BigNum values, but file contents need to be
    String content.

--- ext/create_logic.rb
+++ ext/create_logic.rb
@@ -1,12 +1,22 @@
-(1..100).each do |i|
-  f = File.open("c_00#{i}.pp",'w')
-  if f
-    f.write("class #{ARGV[0]}::c_00#{i} \{\n\n")
-    f.write("include #{ARGV[0]}::c_00#{i - 1}\n\n")
-    (1..10).each do |j|
-      f.write("file \{\"/home/\${id}/#{i}_of_#{j}\": content => fqdn_rand(999999999999999999999999999999),\}\n\n")
+#!/usr/bin/env ruby
+
+# Generate the "logic" file resource puppet manifests, in `manifests/logic`.
+# The default class name is `clamps::logic` but can be overridden on the
+# command line.  The destination path can also be overridden on the command
+# line.
+
+classname  = ARGV.shift || "clamps::logic"
+destination = ARGV.shift || File.expand_path(File.join(File.dirname(__FILE__), "..", "manifests", "logic"))
+
+1.upto(100) do |i|
+  filename = File.join(destination, "c_00#{i}.pp")
+  File.open(filename,'w') do |f|
+    puts "Generating file [#{filename}]..."
+    f.puts "class #{classname}::c_00#{i} {\n\n"
+    f.puts "  include #{classname}::c_00#{i-1}\n\n"
+    1.upto(10) do |j|
+      f.puts %Q|  file {"/home/${id}/#{i}_of_#{j}\": content => "${fqdn_rand(999999999999999999999999999999)}",}|
     end
-    f.write("\}")
-  f.close()
+    f.puts "}"
   end
 end
