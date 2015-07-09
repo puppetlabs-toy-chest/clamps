6b0e63269c1bd5e0588c38cb8bce1097d82a50b4
Rick Bradley <rick@rickbradley.com>
2015-07-08 20:12:35 -0500

    (QENG-2464) Add support for static files
    Prior to this, CLAMPS generated only constantly changing files.
    This method of generating files ends up decreasing the resource
    duplication rate to an unrealistic degree, causing issues for
    load testing, and having negative effects on caching, etc.
    
    Here we're adding the ability to specify a number of static File
    resources to be created. Later we intend to streamline the dynamic
    File resources so they can be specified more predictably.

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,5 +1,14 @@
 class clamps (
-  $logic = '1',
+  $logic            = '1',
+  $num_static_files = 20,
 ) {
+  # create dynamic files
   include "clamps::logic::c_00${logic}"
+
+  # create static files
+  $static_files = clamps_static_files("/home/${id}", $num_static_files)
+  file { $static_files:
+    ensure  => file,
+    content => "This is static file content for file ${name}.",
+  }
 }
